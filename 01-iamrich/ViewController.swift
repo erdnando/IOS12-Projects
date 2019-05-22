77949b39fa63c7aa189376ec496cafafb140f839
Erdnando <erdnando@gmail.com>
2019-05-22 17:04:33 -0500

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Erdnando on 5/22/19.
+//  Copyright © 2019 smi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
