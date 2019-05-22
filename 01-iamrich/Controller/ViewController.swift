24d25509ec655ec0014bbf910282a3ebea77a854
Erdnando <erdnando@gmail.com>
2019-05-22 18:51:51 -0500

    se crearon las carpetas del proyecto MVC

--- 01-iamrich/Controller/ViewController.swift
+++ 01-iamrich/Controller/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
+        /*
+        for i in 1..<10000000 {
+            print("helli \(i)")
+        }*/
+        
     }
 
 
