04def72497cedea85935ecbf869a0b696f466ef1
Erdnando <erdnando@gmail.com>
2019-05-22 21:56:38 -0500

    proyecto 1 finalizado

--- 01-iamrich/Controller/ViewController.swift
+++ 01-iamrich/Controller/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lbltitulo: UILabel!
+    
+    @IBOutlet weak var imgDiamante: UIImageView!
+    
+    @IBOutlet weak var btnProcess: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //test
        
         /*
         for i in 1..<10000000 {
@@ -21,6 +28,36 @@ class ViewController: UIViewController {
         }*/
         
     }
+    
+    
+    @IBAction func btnPressed(_ sender: UIButton) {
+        
+        //print("Boton presionado")
+        
+        let alert:UIAlertController = UIAlertController(title: "Yo soy rico", message: """
+                                                                                        Yo soy rico,
+                                                                                        Yo me lo merezco,
+                                                                                        Yo soy bueno,
+                                                                                        saludable y exitoso
+                                                                                        """, preferredStyle: .actionSheet)
+        
+        
+        let accion:UIAlertAction=UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(accion)
+        
+        let accion2:UIAlertAction=UIAlertAction(title: "Cancelar", style: .cancel) { (action) in
+            print("se ha cancelado la operacion")
+        }
+        
+        alert.addAction(accion2)
+        
+        //present(alert, animated: true, completion:  nil)
+        show(alert, sender: nil)
+        
+    }
+    
+    
 
 
 }
